2e30e543bff754234d69b15025e941d7ab1c0f81
cesarforteslima <cesar@eurotast.eu>
2017-08-28 20:55:11 +0200

    Update tool_METHIS_v7.py

--- METHIS/src/tool_METHIS_v7.py
+++ METHIS/src/tool_METHIS_v7.py
@@ -32,10 +32,8 @@ parser.add_argument('-i', '--input_snp', required=True, type=str, help='Parfile 
 parser.add_argument('-fsc', '--fastsimcoal2', default=None, type=str, help='Running fsc25 and/or converting fsc25 output format. Note about extension files: [inputname].par and [outpoutname]_1_1.arp . Only if Ne and nb_SNPs is different than 1000 and 10000. Please, create your own parfile')
 parser.add_argument('-N', '--nb_SNP', default=50000, type=int, help='Number of independent SNPs, default= 50000')
 parser.add_argument('-Ne', '--Ne', default=1000, type=str, help='Effective population size at foundation, default= 1000/Con/default. Change with Neg0/model[Con, Inc, Dec, or All]/Negn[min-max, or default]')
-parser.add_argument('-s1', '--list_s1', required=True, type=str, help='Parameters for the source population s1: \
-							sourcepop1[proportion of s1g0]/model[Con, Inc, Dec, or All]/trend[min-max, or default]/s1g1[min-max, linear, or default]/s1gn[min-max, or default]')
-parser.add_argument('-s2', '--list_s2', required=True, type=str, help='Parameters for the source population s2: \
-							sourcepop2[proportion of s2g0]/model[Con, Inc, Dec, or All]/trend[min-max, or default]/s2g1[min-max, linear, or default]/s2gn[min-max, or default]')
+parser.add_argument('-s1', '--list_s1', required=True, type=str, help='Parameters for the source population s1: sourcepop1[proportion of s1g0]/model[Con, Inc, Dec, or All]/trend[min-max, or default]/s1g1[min-max, linear, or default]/s1gn[min-max, or default]')
+parser.add_argument('-s2', '--list_s2', required=True, type=str, help='Parameters for the source population s2: sourcepop2[proportion of s2g0]/model[Con, Inc, Dec, or All]/trend[min-max, or default]/s2g1[min-max, linear, or default]/s2gn[min-max, or default]')
 parser.add_argument('-s', '--sampling', default=100/100/100, type=str, help='Sample size sampled for each population (s1, s2, and adm) in the ASD analysis, default= 100')
 parser.add_argument('-r', '--rel_ind', default=0.05, type=str, help='Threshold of proportion of related individuals allowed in the ASD analysis, default= 5 per cent')
 parser.add_argument('-sim', '--nb_simulations', default=10, type=int, help='Number of simulations, default= 10')
